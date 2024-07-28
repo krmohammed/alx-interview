d70a979bc6590559c67926974bfab730998f5fd2
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-28 23:08:24 +0000

    task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    tri = []
+    if n <= 0:
+        return tri
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = tri[i - 1][j - 1] + tri[i - 1][j]
+        tri.append(row)
+    return tri
